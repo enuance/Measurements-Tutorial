dc9a554b22b2ef5041011d1f1eb5586b09185333
Stephen Martinez <enharmonicnuance@gmail.com>
2018-03-21 15:47:28 -0700

    Initial Commit with Tutorial Outline

---
+++ Measurements.playground/Contents.swift
@@ -0,0 +1,32 @@
+/**
+ Measurements Tutorial
+ 
+ Learn Swift SD March 21, 2018
+ Clone me from
+ */
+
+import UIKit
+import PlaygroundSupport
+
+
+PlaygroundPage.current.finishExecution()
+
+//1. What Are Measurements?
+
+//2. What pain points do Measurements solve?
+
+//3. Example of pain point: Radians & Degrees.
+
+//4. How to create a Measurement.
+
+//5. What can Measurements do?
+
+//6. How to create a custom Measurement Type: UnitBromance.
+
+//7. How to customize Measurements to your liking: ie 12.feet.toMeters.cgValue
+
+//8. Measurements in an Animated example.
+
+
+
+
