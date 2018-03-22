ee0453746c96e4a89eb0a3838d6bfa2b017ab7f4
Stephen Martinez <enharmonicnuance@gmail.com>
2018-03-21 22:10:04 -0700

    Added some comments

--- Measurements.playground/Contents.swift
+++ Measurements.playground/Contents.swift
@@ -8,7 +8,8 @@
 import UIKit
 import PlaygroundSupport
 
-//PlaygroundPage.current.finishExecution()
+//Comment this out to run execution
+PlaygroundPage.current.finishExecution()
 
 //1. What Are Measurements?
 
@@ -23,69 +24,65 @@ func radians(degrees: Double) -> Double{
 var degress = Measurement(value: 12, unit: UnitAngle.degrees)
 
 //5. What can Measurements do?
-//degress.converted(to: .radians) * 3
 
+//Returns new Measurment
+degress.converted(to: .radians) * 3
+
+//Mutates the Measurement Struct
 degress.convert(to: .radians)
 
 //6. How to create a custom Measurement Type: UnitBromance.
 class UnitBromance: Dimension{
+    //Static properties as the available UnitBromances
     static let handshake = UnitBromance(symbol: "hshk", converter: UnitConverterLinear(coefficient: 1))
     static let fistbump = UnitBromance(symbol: "fbmp", converter: UnitConverterLinear(coefficient: 5))
     static let chestBump = UnitBromance(symbol: "cbmp", converter: UnitConverterLinear(coefficient: 10))
     static let broHug = UnitBromance(symbol: "bhug", converter: UnitConverterLinear(coefficient: 25))
     static let cryOnMyShoulder = UnitBromance(symbol: "cry", converter: UnitConverterLinear(coefficient: 50))
     
+    //Overriding the class func to allow Conversion
     override public class func baseUnit() -> UnitBromance{
         return UnitBromance.handshake
     }
 }
 
+
+//Using our custom UnitBromance
 let fistBump = Measurement(value: 20, unit: UnitBromance.fistbump)
 fistBump.converted(to: .chestBump)
 fistBump.converted(to: .cryOnMyShoulder)
 
 //7. How to customize Measurements to your liking: ie 12.feet.toMeters.cgValue
 
+//Initial protocol
 protocol SchemaShiftable{
     var entrySchema: Double{get}
     var degrees: Measurement<UnitAngle>{get}
     var radians: Measurement<UnitAngle>{get}
 }
 
+//Provide default implementation
 extension SchemaShiftable{
     var degrees: Measurement<UnitAngle>{return Measurement(value: entrySchema, unit: UnitAngle.degrees)}
-    var radians: Measurement<UnitAngle>{
-        return Measurement(value: entrySchema, unit: UnitAngle.radians)
-    }
+    var radians: Measurement<UnitAngle>{return Measurement(value: entrySchema, unit: UnitAngle.radians)}
 }
 
-extension Int: SchemaShiftable{
-    var entrySchema: Double{return Double(self)}
-    
-    
-}
+//Allows the Int type to have access to Measurment through SchemaShiftable
+extension Int: SchemaShiftable{var entrySchema: Double{return Double(self)}}
 
+//Allows Measurements to return a CGFloat
+extension Measurement{var cgValue: CGFloat{return CGFloat(value)}}
 
-extension Measurement{
-    var cgValue: CGFloat{
-        return CGFloat(value)
-    }
-    
-    
-}
-
+//Provides the Conversion methods to Measurments toUnitType properties
 extension Measurement where UnitType: UnitAngle{
     private var mySelf: Measurement<UnitAngle>{return self as! Measurement<UnitAngle>}
-    var toRadians: Measurement<UnitAngle>{
-        return mySelf.converted(to: .radians)
-    }
-    var toDegrees: Measurement<UnitAngle>{
-        return mySelf.converted(to: .degrees)
-    }
-    
+    var toRadians: Measurement<UnitAngle>{return mySelf.converted(to: .radians)}
+    var toDegrees: Measurement<UnitAngle>{return mySelf.converted(to: .degrees)}
 }
 
+//Example of new syntax
 let something = 12.degrees.toRadians.cgValue
+
 
 //8. Measurements in an Animated example.
 
@@ -99,11 +96,12 @@ let playView = UIView()
 
 containerView.addSubview(playView)
 playView.translatesAutoresizingMaskIntoConstraints = false
+
 [playView.widthAnchor.constraint(equalToConstant: 100),
-playView.heightAnchor.constraint(equalToConstant: 100),
-playView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
-playView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor)
-].forEach{$0.isActive = true}
+ playView.heightAnchor.constraint(equalToConstant: 100),
+ playView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+ playView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor)
+    ].forEach{$0.isActive = true}
 
 playView.backgroundColor = UIColor.purple
 playView.layer.cornerRadius = 25
@@ -111,9 +109,6 @@ playView.clipsToBounds = true
 
 let rotation = CGAffineTransform(rotationAngle: 180.degrees.toRadians.cgValue)
 
-
-UIView.animate(withDuration: 1){
-    playView.transform = rotation
-}
+UIView.animate(withDuration: 1){playView.transform = rotation}
 
 
